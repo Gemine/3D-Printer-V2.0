3d6342e8dd5b4b853bb25d03622ecaeeb7bc5a88
Gemine <nguyenxuanphuc6794@gmail.com>
2019-05-16 10:20:48 +0700

    update

--- TkinterHomeScreen.py
+++ TkinterHomeScreen.py
@@ -13,20 +13,30 @@ root.geometry("1024x600")
 topFrame = Frame(root,bg = "red",width = 600,height = 200)
 topFrame.pack(side = TOP,fill = X)
 
-##loadButton.pack(side = LEFT, padx = 10, pady = 10)
-#loadButton.place(x = 10,y = 10 )
+
+loadButton = Button(topFrame,text = 'LOAD GCODE', width = 10 ,height = 1)
+loadButton.place(x = 20,y = 20 )
 
 gcodeDirLabel = Label(topFrame,text = 'Gcode Directory',width = 80, height = 1 )
-#gcodeDirLabel.pack(side = LEFT)
+gcodeDirLabel.place(x = 150,y = 22)
 
-connectBotton = Button(topFrame,text = 'CONNECT',width = 10 ,height = 1)
-#connectBotton.pack(side = LEFT, anchor = SW)
-connectBotton.place(x = 10,y = 90)
+connectBotton = Button(topFrame,text = 'CONNECT',width = 30 ,height = 1)
+connectBotton.place(x = 20,y = 90)
+
+printButton = Button(topFrame,text = 'PRINT',width = 30 ,height = 1)
+printButton.place(x = 300 ,y = 90)
+
+pauseButton = Button(topFrame,text = 'PAUSE',width = 30 ,height = 1)
+pauseButton.place(x = 580,y = 90)
 
 
 ################# MID FRAME ##################
 midFrame = Frame(root,bg = "blue",width = 600,height = 200)
 midFrame.pack(side = TOP, fill = X)
+
+#Input Box for both
+inputBoxForBoth = Entry(midFrame,width = 50)
+inputBoxForBoth.place(x = 350,y = 20)
 ################ BOTTOM FRAME ################
 bottomFrame = Frame(root,bg = "green",width = 600,height = 200)
 bottomFrame.pack(side = TOP, fill = X)
